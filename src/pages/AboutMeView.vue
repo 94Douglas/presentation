7e95f957996a90b280874812da81ae4ea322992d
Douglas S <94dousch@gafe.molndal.se>
2022-09-07 21:53:11 +0200

    Updates

--- src/pages/AboutMeView.vue
+++ src/pages/AboutMeView.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
+        <br />
+        <section>
+            <h2>Vem är jag?</h2>
+        </section>
+        <br />
+        <img src="../images/me.jpg" alt="Me">
         <base-card>
-            <section>
-                <h2>Vem är jag?</h2>
-            </section>
             <h3>
                 Mitt namn är Douglas Schoug, jag är 28 år gammal.
 
@@ -15,8 +18,38 @@
                 familj och vänner.
                 Annars sitter jag och programmerar så mycket jag kan, just för att jag tycker det är extremt roligt och
                 något jag brinner för.
-                Dock gör jag det även för att
+                Även gör jag det för att
                 utöka min kompetens, främst inom frontend.
+            </h3>
+            <br />
+            <h3>
+                Jag ser mig själv som en teamspelare då grupparbete både är något jag har lång erfarenhet av men också
+                ser som helt nödvändigt för att lösa de olika problem som kan uppstå i systemutveckling, laget före
+                jaget. Man blir aldrig
+                fullärd och tänker ofta att andra vet mer än jag men jag hjälper också gärna mina kollegor och kommer
+                med idéer.
+
+            </h3>
+            <br />
+            <h3>
+                För mig är det viktigt att återanvända kod och inte slösa bort tid till att skapa funktioner som redan
+                finns.
+                Att skriva snygg kod är något jag strävar efter hela tiden, den ska vara lättläst för andra
+                men även för mig själv. En strävan som också finns är välkommenterad kod och responsiv design fär
+                mobiltelefoner.
+            </h3>
+            <br />
+            <h3>
+                När jag utvecklar en hemsida, så utgår jag såklart efter kundens önskemål samt behov.
+                Sedan börjar jag att plenare och strukturera upp projektet.
+                Börja nästan alltid med funktionaliteten och mina tester och sedan börjar jag att applicera min design
+                på hemsidan
+                och utgår ifrån hur jag som användare hade tyckt om på sidan, design och funktion mässigt.
+            </h3>
+            <br />
+            <h3>
+                Jag är en strukturerad person som även är ansvarstagande för min
+                del av projektet, mig kan man lita på när det gäller att leverera mot deadline.
             </h3>
         </base-card>
         <base-card>
@@ -24,7 +57,6 @@
             <h3>
                 I Augusti 2020 bestämde jag mig för att skola om mig och påbörja en ny karriär. Jag började studera till
                 .NET utvecklare, en två årig utbildning på Campus Mölndal.
-
             </h3>
         </base-card>
 
--- src/pages/ProjectView.vue
+++ src/pages/ProjectView.vue
@@ -1,30 +1,85 @@
 <template>
     <div>
+        <br />
+
+        <section>
+            <h1>Vad har jag gjort?</h1>
+        </section>
         <base-card>
             <section>
-                <h2>Vad har jag gjort?</h2>
+                <h2>Projekt 1</h2>
             </section>
+            <br />
 
-            <h2>Har gjort två projekt åt två kunder.
-                Första projektet var en hemsida till en bostadsrättsförening.
-                Då vi hade examensarbete det absolut sista i utbildningen, såg jag min chans att påbörja detta projekt.
+            <h3>Har gjort två projekt åt två kunder.
+                Första projektet är en hemsida till en bostadsrättsförening.
+                Examensarbetet var det sista momentet att utföra i utbildningen, här såg jag min chans att påbörja detta
+                projekt.
                 Då examensarbetet gick ut på att lära sig en helt ny teknik, valde jag mig för att lära mig Vue.js.
                 Det lät som ett väldigt intressant verktyg och populariteten kring Vue växte, men att kunna mer än ett
                 verktyg
                 var verkligen av intresse.
-            </h2>
+            </h3>
             <br />
-            <h2>
+            <h3>
                 Det var en lärorik väg att gå, termonologin var ej så olik andra språk, så som React. Eftersom vi hade
                 haft
                 en kurs inom React, blev det lite lättare att sätta sig in i verktyget Vue.
-                Vill även lägga en notering att denna hemsidan är gjord i Vue programmeringsspråk.
-            </h2>
+                Har återanvänt komponenter på hela projektet och kommenterad kod. Detta resulterade i att man sparade
+                extremt mycket tid
+                samt
+                sparade in hur stort hela projektet skulle bli.
+            </h3>
             <br />
-            <h2>
+            <h3>
+                Syftet med Bostadsrättsföreningens hemsida är att man ska kunna läsa lite information om området och
+                bostadsrättsföreningen.
+                Kunna läsa årsredovisning. På hemsidan skulle man kunna registrera en användare, login system och kunna
+                göra ett inlägg på hemsidan under fliken
+                årsredovisning.
+            </h3>
+            <br />
+            <h3>
+                I framtiden har de planer på att det ska finnas funktionalitet, så de kan se en översikt på
+                parkeringsplatser.
+                Vilken plats är upptagen och vilken är ledig, då det finns begränsade antal parkeringsplatser.
+
+            </h3>
+            <br />
+            <h3>
                 Länken till första projektet finner ni genom att klicka
-                <a v-on:click="openInNewTab()" href="">HÄR</a>
-            </h2>
+                <a v-on:click="openBrfInNewTab()" href="">HÄR</a>
+            </h3>
+
+        </base-card>
+
+        <base-card>
+            <section>
+                <h2>Projekt 2</h2>
+            </section>
+            <br />
+            <h3>
+                Det andra projektet jag utförde till en kund var en hemsida till en samfällighet.
+                Då jag är väldigt intresserad av nya tekniker, såg jag chansen att lära mig något nytt.
+                Den nya tekniken som jag valde var att lära mig var MERN fullstack lösning.
+                Kunden önskade att man skulle kunna skapa konton, ett login system, kunna lägga till eller ta bort
+                information
+                på "Kontakt" fliken.
+                Ett system för felanmälningar samt kunna ladda upp PDF fil för årsredovisningen var även ett önskemål,
+                som även utfördes.
+            </h3>
+            <br />
+            <h3>
+                För att inte alla användare skulle ha tillgång till administratörernas funktionaliteter,
+                finns det möjlighet för att en ny användare ska vara en admin eller vanlig användare.
+                Admin får då också tillgång till en admin panel, som göms för vanliga användare. Skulle
+                en vanlig användare veta länken till admin panelen, så kommer de att nekas då de inte har behörighet
+                till denna delen.
+            </h3>
+            <br />
+            <h3>Länken till andra projektet finner ni genom att klicka
+                <a v-on:click="openSamfallighetInNewTab()" href="">HÄR</a>
+            </h3>
 
         </base-card>
 
@@ -36,11 +91,15 @@ export default {
     data() {
         return {
             brfHallungen: "https://brfhallungen.se/",
+            samfallighet: "http://olovshagesamfallighet.se/",
         }
     },
     methods: {
-        openInNewTab() {
+        openBrfInNewTab() {
             window.open(this.brfHallungen, "_blank", "noopener,noreferrer");
+        },
+        openSamfallighetInNewTab() {
+            window.open(this.samfallighet, "_blank", "noopener,noreferrer");
         },
     }
 }
